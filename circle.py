6cd281b984300353cb1ce3940fb46e4462119cc7
Ralf Schlatterbeck <rsc@runtux.com>
2017-07-22 20:19:39 +0200

    circle of fifth

---
+++ circle.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+
+from contrapunctus.halftone import Halftone
+
+class Circle_Of_Fifth (object) :
+    fifth_up = dict \
+        (( ( 'C',   'G')
+         , ('^C',  '^G')
+         , ( 'D',   'A')
+         , ('^D',  '^A')
+         , ( 'E',   'B')
+         , ( 'F',   'c')
+         , ('^F',  '^c')
+         , ( 'G',   'd')
+         , ('^G',  '^d')
+         , ( 'A',   'e')
+         , ('^A',  '^e')
+         , ( 'B',  '^f')
+         , ( 'c',   'g')
+         , ('^c',  '^g')
+         , ( 'd',   'a')
+         , ('^d',  '^a')
+         , ( 'e',   'b')
+         , ( 'f',   "c'")
+         , ('^f',  "^c'")
+         , ( 'g',   "d'")
+         , ('^g',  "^d'")
+         , ( 'a',   "e'")
+         , ('^a',  "^e'")
+         , ( 'b',  "^f'")
+        ))
+    fifth_down = dict \
+        (( ( 'C',   'F,')
+         , ('_D',  '_G,')
+         , ( 'D',   'G,')
+         , ('_E',  '_A,')
+         , ( 'E',   'A,')
+         , ( 'F',  '_H,')
+         , ('_G',  '_C')
+         , ( 'G',   'C')
+         , ('_A',  '_D')
+         , ( 'A',   'D')
+         , ('_B',  '_E')
+         , ( 'B',   'E')
+         , ( 'c',   'F')
+         , ( 'd',   'G')
+         , ('_d',  '^G')
+         , ( 'e',   'A')
+         , ('_e',  '_A')
+         , ( 'f',  '_H')
+         , ('_g',  '_c')
+         , ( 'g',   'c')
+         , ('_a',  '_d')
+         , ( 'a',   'd')
+         , ('_b',  '_e')
+         , ( 'b',   'e')
+        ))
+
+    @classmethod
+    def transpose_quint_up (cls.h) :
+        return Halftone (cls.fifth_up [h.name])
+    # end def transpose_quint_up
+
+    @classmethod
+    def transpose_quint_down (cls.h) :
+        return Halftone (cls.fifth_down [h.name])
+    # end def transpose_quint_down
+
+# end class Circle_Of_Fifth
